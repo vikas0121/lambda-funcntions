de257a7e9a9bf180ae50d84cc7c70293caaf4a94
Vikas Kumar <vikas.kumar@freecharge.com>
2020-08-20 18:04:31 +0530

    adding rest api

--- functions/pdf.js
+++ functions/pdf.js
@@ -1,16 +1,22 @@
-const middy = require('middy');
+// const middy = require('middy');
 const chromium = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer-core');
-const { cors, doNotWaitForEmptyEventLoop, httpHeaderNormalizer, httpErrorHandler } = require('middy/middlewares');
+// const { cors, doNotWaitForEmptyEventLoop, httpHeaderNormalizer, httpErrorHandler } = require('middy/middlewares');
 
 module.exports.generatePdf = async event => {
+  console.log("event", event)
+  // const data = JSON.parse(event.body)
+  // console.log("data", data)
 
-  const data = JSON.parse(event.body)
-
-  const { htmlString, options } = data
+  const { htmlString, options } = event
   const { format = 'A4', margin = { top: 5 }, printBackground = true } = options
-
-  const browser = await puppeteer.launch()
+  const executablePath = event.isOffline
+    ? './node_modules/puppeteer/.local-chromium/mac-674921/chrome-mac/Chromium.app/Contents/MacOS/Chromium'
+    : await chromium.executablePath
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    executablePath
+  })
   const page = await browser.newPage()
   await page.setContent(htmlString, { waitUntil: 'networkidle0' })
 
